d9d7844acebc5f5816454371d4ae78cc53090d41
Kunal Gole <pragatipatil091@gmail.com>
2025-04-04 13:27:31 +0530

    scrolllbar fixed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -9,7 +9,7 @@ const SignUpPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const { signup } = useAuthStore();
+  const { signup, isSigningUp } = useAuthStore();
   
   const handleSignup = (e) => {
     e.preventDefault();
@@ -70,8 +70,10 @@ const SignUpPage = () => {
 
               <button className="w-full py-2 bg-red-600 text-white
               font-semibold rounded-md hover:bg-red-700
-              ">
-                Sign Up
+              "
+              disabled={isSigningUp}
+              >
+                {isSigningUp ? "Loading..." : "Sign Up"}
               </button>
           </form>
           <div className="text-center text-gray-400">
